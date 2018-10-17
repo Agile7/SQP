ee8d7648995b53e5449eb2f0be1493db925f664f
viloshnaS <39422333+viloshnaS@users.noreply.github.com>
2018-10-17 18:50:24 +0200

    R9 - Get Unread codes

---
+++ CodeReviewClient/src/main/java/com/agileseven/codereview/client/listeners/CodeListMouseListener.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agileseven.codereview.client.listeners;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author vilosh_na
+ */
+public class CodeListMouseListener implements MouseListener {
+
+    public CodeListMouseListener() {
+    }
+    
+    
+    
+    @Override
+     public void mouseClicked(MouseEvent e) {
+       System.out.println("csdcsd");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+    
+}
