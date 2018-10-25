21766dd0699df91d97ba89b5eac6a01f14ba3545
viloshnaS <39422333+viloshnaS@users.noreply.github.com>
2018-10-26 00:55:19 +0200

    disable click for user's codes

--- CodeReviewClient/src/main/java/com/agileseven/codereview/client/views/FrameUnreadCodes.java
+++ CodeReviewClient/src/main/java/com/agileseven/codereview/client/views/FrameUnreadCodes.java
@@ -8,22 +8,11 @@ package com.agileseven.codereview.client.views;
 import com.agileseven.codereview.client.DTO.CodeDTO;
 import com.agileseven.codereview.client.DTO.UserDTO;
 import com.agileseven.codereview.client.ServiceConsumer;
-import com.agileseven.codereview.client.listeners.CodeListMouseListener;
 import com.agileseven.codereview.client.utils;
 import com.agileseven.codereview.client.DTO.UserstoryDTO;
 import com.agileseven.codereview.client.Session;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Label;
 import java.util.ArrayList;
 import java.util.Date;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -33,7 +22,7 @@ import javax.swing.table.DefaultTableModel;
 public class FrameUnreadCodes extends javax.swing.JFrame {
     
     ServiceConsumer service = new ServiceConsumer();
-    
+    ArrayList<CodeDTO> codeList;
 
     /**
      * Creates new form UnreadCodeList
@@ -43,7 +32,7 @@ public class FrameUnreadCodes extends javax.swing.JFrame {
         initComponents();
         jLabel4.setText(Session.currentUser.getFirstName() + " " + Session.currentUser.getLastName());
         
-        ArrayList<CodeDTO> codeList = service.getUnreadCodes(Session.currentProject.getProjectId());
+        codeList = service.getUnreadCodes(Session.currentProject.getProjectId());
         
         DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
         jTable1.getColumnModel().getColumn(0).setMaxWidth(75);
@@ -51,6 +40,7 @@ public class FrameUnreadCodes extends javax.swing.JFrame {
         jTable1.getColumnModel().getColumn(2).setWidth(500);
         jTable1.getColumnModel().getColumn(3).setWidth(500);
         jTable1.getColumnModel().getColumn(4).setWidth(500);
+       
 
         
         if(codeList != null && codeList.size() > 0){
@@ -67,27 +57,18 @@ public class FrameUnreadCodes extends javax.swing.JFrame {
                 model.addRow(new Object[]{code.getCodeId(),code.getUserStoryId() + " " + userStory.getTitle(),
                     user.getFirstName()+ " " + user.getLastName(),
                     code.getComment() , utils.dateDiff(code.getPushDate(),currentDate)});
+
+         
             }
         }
         else{
-            
-           
+            jTable1.setVisible(false);
+            jLabel2.setVisible(true);
         }
-        
-        jTable1.getSelectionModel().addListSelectionListener((ListSelectionEvent event) -> {
-            changeScreen(Integer.parseInt(jTable1.getValueAt(jTable1.getSelectedRow(), 0).toString()));
-        });
-        
-     
-        
+
     }
     
-    
-    public void changeScreen(int codeId){
-        new FrameReview(codeId).setVisible(true);
-        this.setVisible(false);
-        
-    }
+ 
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -132,6 +113,9 @@ public class FrameUnreadCodes extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel2.setVisible(false);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -165,23 +149,48 @@ public class FrameUnreadCodes extends javax.swing.JFrame {
         jTable1.setColumnSelectionAllowed(false);
         jTable1.setGridColor(new java.awt.Color(255, 255, 255));
         jTable1.setRowHeight(40);
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tableClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
+
+        jButton1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(204, 0, 51));
+        jButton1.setText("Back");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 153, 0));
+        jLabel2.setText("No pending codes to review");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(521, 521, 521)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
                 .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(51, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel4)
-                        .addGap(121, 121, 121))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(55, Short.MAX_VALUE))))
+                        .addGap(121, 121, 121))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -190,23 +199,40 @@ public class FrameUnreadCodes extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(jLabel4))
-                .addGap(35, 35, 35)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 679, Short.MAX_VALUE)
-                .addGap(65, 65, 65))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 592, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
- private void backActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
+    private void tableClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableClicked
         
+        
+        int selectedIndex = jTable1.getSelectedRow();
+        
+        if(codeList.get(selectedIndex).getUserId() != Session.currentUser.getUserId()){
+            new FrameReview(Integer.parseInt(jTable1.getValueAt(selectedIndex, 0).toString())).setVisible(true);
+            this.setVisible(false);
+        }
+    }//GEN-LAST:event_tableClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
         this.setVisible(false);
         new FrameHomePage().setVisible(true);
-    }   
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
