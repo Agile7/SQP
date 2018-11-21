08b0321bcabedcd3e6c5e9e99a2176bcf7415f3c
XutongJin <rosa.jin.cueb@hotmail.com>
2018-11-21 11:04:08 +0100

    PersonalDashboard Backend Unfinished

---
+++ CodeReviewServer/src/main/java/com/agileseven/codereviewserver/Controller/PersonalDashboardController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agileseven.codereviewserver.Controller;
+
+import com.agileseven.codereviewserver.DAO.GamificationDAO;
+import com.agileseven.codereviewserver.DAO.GamificationDAOImpl;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Jin Xutong
+ */
+
+@RestController
+@RequestMapping(path="/CodeReviewer")
+public class PersonalDashboardController {
+    
+}
---
+++ CodeReviewServer/src/main/java/com/agileseven/codereviewserver/DAO/PersonalDashboardDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agileseven.codereviewserver.DAO;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ *
+ * @author Jin Xutong
+ */
+public interface PersonalDashboardDAO {
+    
+    public LinkedHashMap<String, Integer> getNumberOfPersonalCodeRejected(String startDate, String endDate, int period, int projectId, int userId);
+    public LinkedHashMap<String, Integer> getNumberOfPersonalCodeApproved(String startDate, String endDate, int period, int projectId, int userId);
+    
+}
