1ec6ef76250a4d7e26aff438c9f2f8a4454aa3e2
XutongJin <rosa.jin.cueb@hotmail.com>
2018-11-21 15:22:17 +0100

    Update PersonalDashboardController.java

--- CodeReviewServer/src/main/java/com/agileseven/codereviewserver/Controller/PersonalDashboardController.java
+++ CodeReviewServer/src/main/java/com/agileseven/codereviewserver/Controller/PersonalDashboardController.java
@@ -5,8 +5,8 @@
  */
 package com.agileseven.codereviewserver.Controller;
 
-import com.agileseven.codereviewserver.DAO.GamificationDAO;
-import com.agileseven.codereviewserver.DAO.GamificationDAOImpl;
+import com.agileseven.codereviewserver.DAO.PersonalDashboardDAO;
+import com.agileseven.codereviewserver.DAO.PersonalDashboardDAOImpl;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +22,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="/CodeReviewer")
 public class PersonalDashboardController {
+    private PersonalDashboardDAO personalDashboardDAO = new PersonalDashboardDAOImpl();
+    
+    @RequestMapping(path = "/numCodeRejected", method=RequestMethod.GET)
+    public LinkedHashMap<String, Integer> numberOfPersonalCodeRejected(@RequestParam(value="startDate", defaultValue="") String startDate,
+                        @RequestParam(value="endDate", defaultValue="") String endDate,
+                        @RequestParam(value="period", defaultValue="") int period,
+                        @RequestParam(value="userId", defaultValue="") int userId) {
+        
+        LinkedHashMap<String, Integer> map = null;
+        try{
+           map = personalDashboardDAO.getNumberOfPersonalCodeRejected(startDate, endDate, period, userId);
+       
+        }
+        catch(Exception ex){
+//            System.err.println("Got an exception!");
+//            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        
+        
+        return map;
+    }
+    
+    @RequestMapping(path = "/numCodeApproved", method=RequestMethod.GET)
+    public LinkedHashMap<String, Integer> numberOfPersonalCodeApproved(@RequestParam(value="startDate", defaultValue="") String startDate,
+                        @RequestParam(value="endDate", defaultValue="") String endDate,
+                        @RequestParam(value="period", defaultValue="") int period,
+                        @RequestParam(value="userId", defaultValue="") int userId) {
+        
+        LinkedHashMap<String, Integer> map = null;
+        try{
+           map = personalDashboardDAO.getNumberOfPersonalCodeApproved(startDate, endDate, period, userId);
+       
+        }
+        catch(Exception ex){
+//            System.err.println("Got an exception!");
+//            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        
+        return map;
+    }
     
 }
